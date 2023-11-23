990c557087b172f7eb1f0b67c51b307e6319fcde
goda0204 <151479525+goda0204@users.noreply.github.com>
2023-11-23 09:45:55 +0900

    Update bugs_music.py

--- 0. 실습/bugs_music.py
+++ 0. 실습/bugs_music.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 class BugsMusic:
     def __init__(self):
-        self.domain='https://music.bugs.co.kr'
+        self.domain = 'https://music.bugs.co.kr'
         self.url = ''
         self.headers = {'User-Agent': 'Mozilla/5.0'}
         self.class_name = []
@@ -12,27 +12,27 @@ class BugsMusic:
         self.dict = {}
         self.df = None
 
-    def set_url(self,url):
+    def set_url(self, url):
         self.url = requests.get(f'{self.domain}/{url}', headers=self.headers).text
 
     def get_url(self):
         return self.url
 
-    def get_ranking(self):
+    def get_raking(self):
         soup = BeautifulSoup(self.url, 'lxml')
-        ls = soup.find_all(name='p', attrs=({"class":'title'}))
-        for i in ls:
+        ls1 = soup.find_all(name='p', attrs=({"class":'title'}))
+        for i in ls1:
             self.title_ls.append(i.find("a").text)
         return self.title_ls
 
-    def get_ranking(self):
+    def get_artist(self):
         soup = BeautifulSoup(self.url, 'lxml')
-        ls2 = soup.find_all(name='p', attrs=({"class": 'artist'}))
-        for i in ls2:
+        ls1 = soup.find_all(name='p', attrs=({"class": 'artist'}))
+        for i in ls1:
             self.artist_ls.append(i.find("a").text)
         return self.artist_ls
 
-    def insert_dict(self):
+    def insert_dict(self): # 리스트 데이터를 딕셔너리로 옮긴다. 왜냐하면 엑셀로 저장하기 위해...
         for i, j in enumerate(self.title_ls):
             self.dict[j] = self.artist_ls[i]
 
@@ -46,18 +46,17 @@ class BugsMusic:
 if __name__ == '__main__':
     b = BugsMusic()
     url = input('크롤링 대상 url 을 입력하시오.')
-    b.set_url(url)
     # https://music.bugs.co.kr/chart/track/day/total
     b.set_url(url)
     u2 = b.get_url()
 
     print(f'당신이 원하는 url 은 {u2} 입니다.')
 
-    ls = b.get_ranking()
+    ls = b.get_raking()
 
     print(f'노래제목 리스트 확인 {ls}')
 
-    ls2 = b.get_ranking()
+    ls2 = b.get_artist()
 
     print(f'가수 리스트 확인 {ls2}')
 
